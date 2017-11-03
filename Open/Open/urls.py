c53455a55ea68eb35680e381763f6c0cda241f93
lanstine <laanstine@mavs.coloradomesa.edu>
2017-11-02 19:58:30 -0600

    Start of Homepage. Builds, loads, doesn't link.

--- Open/Open/urls.py
+++ Open/Open/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    url(r'^$', 'home.views.home', name='home'),
+    url(r'^', include('home.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
