feb44ca2514fb0adb74910f139c2aaf526da2001
lanstine <laanstine@mavs.coloradomesa.edu>
2017-11-29 18:59:42 -0700

    Fixed login page, added some css

--- Open/accounts/urls.py
+++ Open/accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'accounts:login'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'index'}, name='logout'),
 
 
 ]
