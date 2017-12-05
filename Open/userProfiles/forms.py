605c32e48793a1274e51343a3cc03f73fa12bc62
cristina <cristinabustamante@aol.com>
2017-12-05 00:36:18 -0700

    fixed view error for questionnaire , changed register redirect to the questionnaire

--- Open/userProfiles/forms.py
+++ Open/userProfiles/forms.py
@@ -6,11 +6,17 @@ from .models import Interests, UserProfile, GenderPreference
 
 
 class UserForm(forms.ModelForm):
-    user_gender_pref = forms.ModelMultipleChoiceField(queryset=GenderPreference.objects.all(), widget=forms.CheckboxSelectMultiple())
+    user_gender_pref = forms.ModelMultipleChoiceField(queryset=GenderPreference.objects.all(), widget=forms.CheckboxSelectMultiple(),label='Gender Preference')
     class Meta:
         model = UserProfile
-        fields = ('user_location', 'user_birthday', 'user_gender','user_gender_pref', 'partner_location_preference')
-
+        fields = ('user_location', 'user_birthday', 'user_gender','user_gender_pref', 'partner_location_preference',)
+        labels = {
+            'user_location': ('Location'),
+            'user_birthday': ('Birthday'),
+            'user_gender': ('Gender'),
+            'user_gender_pref':('Gender Preference'),
+            'partner_location_preference':('Look For Match In'),
+        }
 
 class InterestsForm(forms.ModelForm):
     interest_type = forms.ModelMultipleChoiceField(queryset=Interests.objects.all(), widget=forms.CheckboxSelectMultiple())
